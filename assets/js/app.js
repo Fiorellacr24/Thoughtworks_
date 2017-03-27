3e5b1679e053283cd80fb5996f96826c9224b29b
Fiorellacr24 <fiorella.0624@gmail.com>
2017-03-27 01:57:05 -0500

    popover

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,8 @@
-var resources = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  local: ["Windows", "Ubuntu", "Firefox3", "CoreDuo", "Chrome"]
-});
-
-  function showPopOver() {
-    return '<input type="text" class="form-control"/>'
-           '<button class="btn btn-default" type"submit">Add resources</button>'
-           '<button class="btn btn-default" type"submit">Close</button>';
+function showPopOver() {
+    return '<p>(separate multiple resources name with commas)</p>' +
+           '<input type="text" class="form-control"/>' + 
+           '<button class="btn btn-primary" type"submit">Add resources</button>' + " " +
+           '<button class="btn btn-danger" type"submit">Close</button>';
   }
 
   $(function() {
@@ -20,15 +15,5 @@ var resources = new Bloodhound({
         selector: '.panel',
         padding: 0
       }
-    }).on('inserted.bs.popover', function() {
-      $('[data-role="tagsinput"]').tagsinput({
-        typeaheadjs: [{
-          hint: true,
-          highlight: true,
-        }, {
-          name: 'resourceList',
-          source: resources
-        }]
-      });
     })
   });
