276d1c4ddc24c94cd00182ff58f811b05ca27bda
Fiorella Cisneros <fiorella.0624@gmail.com>
2017-03-28 16:42:30 -0500

    function_Bloodhound

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,29 @@
+  var resources = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  local: ["Windows", "Ubuntu", "Firefox3", "CoreDuo", "Chrome", "MySQL"]
+});
+
 function showPopOver() {
     return '<p>(separate multiple resources name with commas)</p>' +
-           '<input type="text" class="form-control"/>' + 
+           '<input type="text" class="form-control" data-role="tagsinput" />' + '<br>' +
            '<button id="js-add-button" class="btn btn-primary" type"submit">Add resources</button>' + " " +
            '<button class="btn btn-danger" type"submit">Close</button>';
   }
 
   $(function() {
-    $('.resourcePopOver').popover({
-      content: showPopOver,
-      container: 'body',
-      placement: 'bottom',
-      html: true,
-      viewport: {
-        selector: '.panel',
-        padding: 0
+  $('.resourcePopOver').popover({
+    content: showPopOver,
+    container: 'body',
+    viewport: { selector: 'body', padding: 0 }
+  }).on('inserted.bs.popover', function() {
+    $('[data-role="tagsinput"]').tagsinput({
+      typeaheadjs: {
+        name: 'resourceList',
+        source: resources
       }
-    })
-  });
+    });
+  })
+});
 
   
